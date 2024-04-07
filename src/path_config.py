bbba1376dd92b64d2f4f085d8d5aa1a33f3d3838
Ali Altıparmak <ali@huridocs.org>
2024-04-07 06:39:23 +0300

    Update paths

--- src/path_config.py
+++ src/path_config.py
@@ -1,2 +1,6 @@
-MODELS_PATH = "../models"
-WORD_GRIDS_PATH = "../word_grids"
+from os.path import join
+from pathlib import Path
+
+PROJECT_ROOT_PATH = Path(__file__).parent.parent
+MODELS_PATH = join(PROJECT_ROOT_PATH, "models")
+WORD_GRIDS_PATH = join(PROJECT_ROOT_PATH, "word_grids")
