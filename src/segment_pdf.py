9dabdcfdbf0f95b4cf74cdbd42860fcb27c5bace
Ali Altıparmak <ali@huridocs.org>
2024-04-16 16:48:39 +0300

    Update segment pdf

--- src/segment_pdf.py
+++ src/segment_pdf.py
@@ -3,8 +3,9 @@ from PdfImages import PdfImages
 from download_models import download_models
 from create_word_grid import create_word_grid
 from get_json_annotations import get_annotations
-from performance import get_performance
 from predict import predict
+from src.get_most_probable_pdf_segments import get_most_probable_pdf_segments
+from src.save_output_to_pdf import save_output_to_pdf
 
 
 def get_segmentation(pdf_path: str):
@@ -13,9 +14,9 @@ def get_segmentation(pdf_path: str):
     create_word_grid(pdf_images.pdf_features)
     get_annotations(pdf_images)
     predict()
-    get_performance(pdf_images.pdf_features.file_name)
+    get_most_probable_pdf_segments(pdf_images.pdf_features)
+    save_output_to_pdf(pdf_path, pdf_images)
 
 
 if __name__ == '__main__':
     get_segmentation(sys.argv[1] if len(sys.argv) >= 2 else None)
-    # get_segmentation(/home/ali/Desktop/pdf-labeled-data/pdfs/cejil8/document.pdf)
