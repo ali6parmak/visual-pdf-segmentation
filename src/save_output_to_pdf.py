d2986b7b178c8e5713584258fec46fc6616a73e4
Ali Altıparmak <ali@huridocs.org>
2024-04-16 16:28:25 +0300

    Load predicted pdf segments to save them to PDF

--- src/save_output_to_pdf.py
+++ src/save_output_to_pdf.py
@@ -1,10 +1,10 @@
-import json
+import pickle
 from os import makedirs
 from os.path import join
-from pathlib import Path
+from paragraph_extraction_trainer.PdfSegment import PdfSegment
 from pdf_annotate import PdfAnnotator, Location, Appearance
 from src.PdfImages import PdfImages
-from src.configuration import D4LA_TYPES_TO_TOKEN_TYPES, COLOR_BY_TOKEN_TYPE
+from src.configuration import CATEGORY_BY_ID, COLOR_BY_CATEGORY
 from src.path_config import PROJECT_ROOT_PATH
 
 
@@ -14,30 +14,34 @@ def hex_color_to_rgb(color: str):
     return r/255, g/255, b/255, alpha
 
 
-def add_prediction_annotation(annotator: PdfAnnotator, annotation: dict, page_height: int):
-    bbox = annotation['bbox']
-    predicted_type = D4LA_TYPES_TO_TOKEN_TYPES[annotation['category_id']].name
-    color = COLOR_BY_TOKEN_TYPE[D4LA_TYPES_TO_TOKEN_TYPES[annotation['category_id']]]
-    left, top, right, bottom = bbox[0], page_height-bbox[1], bbox[0] + bbox[2], page_height-(bbox[1]+bbox[3])
+def add_prediction_annotation(annotator: PdfAnnotator, predicted_segment: PdfSegment, page_height: int):
+    bbox = predicted_segment.bounding_box
+    predicted_type = CATEGORY_BY_ID[predicted_segment.segment_type]
+    color = COLOR_BY_CATEGORY[predicted_type]
+    left, top, right, bottom = bbox.left, page_height-bbox.top, bbox.left + bbox.width, page_height-(bbox.top+bbox.height)
     annotator.add_annotation('square',
-                             Location(x1=left, y1=bottom, x2=right, y2=top, page=annotation['image_id']),
+                             Location(x1=left, y1=bottom, x2=right, y2=top, page=predicted_segment.page_number-1),
                              Appearance(stroke_color=hex_color_to_rgb(color))
                              )
 
     annotator.add_annotation('text',
-                             Location(x1=left, y1=bottom, x2=left + 100,y2=top+bbox[3], page=annotation['image_id']),
+                             Location(x1=left, y1=bottom, x2=left + 100,y2=top+bbox.height, page=predicted_segment.page_number-1),
                              Appearance(content=predicted_type, font_size=10, fill=hex_color_to_rgb(color))
                              )
 
 
+def get_predicted_segments() -> list[PdfSegment]:
+    predicted_segments_path = join(str(PROJECT_ROOT_PATH), "model_output", "predicted_segments.pickle")
+    with open(predicted_segments_path, "rb") as f:
+        return pickle.load(f)
+
+
 def save_output_to_pdf(pdf_path: str, pdf_images: PdfImages):
     annotator = PdfAnnotator(pdf_path)
-    annotations_path: str = join(str(PROJECT_ROOT_PATH), "model_output", "inference", "coco_instances_results.json")
-    annotations = json.loads(Path(annotations_path).read_text())
-    for annotation in annotations:
-        if annotation["score"] < 0.5:
-            continue
-        add_prediction_annotation(annotator, annotation, pdf_images.pdf_images[annotation["image_id"]].height)
+    predicted_segments: list[PdfSegment] = get_predicted_segments()
+    for predicted_segment in predicted_segments:
+        page_height = pdf_images.pdf_features.pages[predicted_segment.page_number-1].page_height
+        add_prediction_annotation(annotator, predicted_segment, page_height)
     pdf_outputs_path = join(PROJECT_ROOT_PATH, "pdf_outputs")
     makedirs(pdf_outputs_path, exist_ok=True)
     output_pdf_path = join(PROJECT_ROOT_PATH, "pdf_outputs", pdf_images.pdf_features.file_name + ".pdf")
