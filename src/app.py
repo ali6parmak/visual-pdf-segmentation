7a9f4c8b265e692428942697ca6d90f451d3bd39
Gabo <gabriel.piles.glez@gmail.com>
2024-05-02 17:42:17 +0200

    Create docker container

---
+++ src/app.py
@@ -0,0 +1,39 @@
+
+import sys
+import tempfile
+import uuid
+from os.path import join
+from pathlib import Path
+
+from fastapi import FastAPI, HTTPException, UploadFile, File
+
+from src.segment_pdf import get_segmentation
+
+app = FastAPI()
+
+
+def get_file_path(file_name, extension):
+    return join(tempfile.gettempdir(), file_name + "." + extension)
+
+
+def pdf_content_to_pdf_path(file_content):
+    file_id = str(uuid.uuid1())
+
+    pdf_path = Path(get_file_path(file_id, "pdf"))
+    pdf_path.write_bytes(file_content)
+
+    return pdf_path
+
+
+@app.get("/")
+async def info():
+    return sys.version
+
+
+@app.post("/get_paragraphs")
+async def get_paragraphs(file: UploadFile = File(...)):
+    try:
+        pdf_path = pdf_content_to_pdf_path(file.file.read())
+        return get_segmentation(model_name="doclaynet", pdf_paths=[str(pdf_path)])
+    except Exception:
+        raise HTTPException(status_code=422, detail="Error extracting paragraphs")
--- src/segment_pdf.py
+++ src/segment_pdf.py
@@ -1,5 +1,6 @@
 import argparse
 from PdfImages import PdfImages
+from data_model.SegmentBox import SegmentBox
 from download_models import download_models
 from create_word_grid import create_word_grid
 from get_json_annotations import get_annotations
@@ -14,9 +15,10 @@ def get_segmentation(model_name: str, pdf_paths: list[str]):
     create_word_grid([pdf_images.pdf_features for pdf_images in pdf_images_list])
     get_annotations(pdf_images_list)
     predict(model_name)
-    predicted_segments = get_most_probable_pdf_segments(model_name, pdf_images_list, False)
-    save_output_to_pdf(model_name, pdf_paths, pdf_images_list, predicted_segments)
 
+    predicted_segments = get_most_probable_pdf_segments(model_name, pdf_images_list, False)
+    # save_output_to_pdf(model_name, pdf_paths, pdf_images_list, predicted_segments)
+    return [SegmentBox.from_pdf_segment(pdf_segment).to_dict() for pdf_segment in predicted_segments]
 
 def get_args():
     parser = argparse.ArgumentParser(description="Process PDF segmentation")
